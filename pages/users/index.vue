e8477a98d90e79e0edcf43b20d60783363c5f8c3
Alexandr <sasha-is-permi@mail.ru>
2020-12-10 01:30:20 +0500

    '4'

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,7 +3,11 @@
     <h1>Users page</h1>
     <div class="menu">
     <ul>
+
+      <!-- Выводим список из элементов массива -->
       <li v-for="user of users" :key="user">
+        <!-- При клике переходим без перезагрузки страницы на другую 
+        страничку- с User (goTO(user)- метод, описанный ниже)-->
         <a href="#" @click.prevent="goTo(user)">User {{user}}</a>
       </li>
     </ul>
@@ -19,6 +23,9 @@ export default {
     ]
   }),
   methods: {
+    // Программная (динамическая) навигация.
+    // программно определяем что нужно переходить на страничку
+    // users/1
     goTo(user) {
       this.$router.push('/users/' + user)
     }
