b139449c0088c1aa925061a6172063d7cefb8a92
Alexandr <sasha-is-permi@mail.ru>
2020-12-15 14:08:51 +0500

    '10'

---
+++ Project1/pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, omnis.</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam reprehenderit, corrupti recusandae quae ducimus consectetur.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
