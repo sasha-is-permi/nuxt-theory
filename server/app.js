c957859c63c797ace0c455f0f1d61e0a1ed14eee
Alexandr <sasha-is-permi@mail.ru>
2021-01-11 17:02:57 +0500

    '21'

--- server/app.js
+++ server/app.js
@@ -4,11 +4,30 @@ const mongoose = require('mongoose')
 const keys = require('./keys')
 const app = express()
 
-mongoose.connect(keys.MONGO_URI)
-  .then(() => console.log('MongoDB connected...'))
-  .catch(error => console.error(error))
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
 
-module.exports = app
+
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = keys.MONGO_URI;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+
+  const collection = client.db("test").collection("devices");
+   console.log('Database connect')
+   // perform actions on the collection object
+   app.use(bodyParser.urlencoded({extended: true}))
+   app.use(bodyParser.json())
+   
+   module.exports = app
+
+
+  client.close();
+  });
+
+ //   mongoose.connect(keys.MONGO_URI) 
+ // .then(() => console.log('MongoDB connected...'))
+//.catch(console.log('MongoDB not connected...'))
+//app.use(bodyParser.urlencoded({extended: true}))
+//app.use(bodyParser.json())
+
+//module.exports = app
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,3 +1,8 @@
+
+
+
 module.exports = {
-  MONGO_URI: `mongodb+srv://sasha2020:qwe123***@cluster0.kx3t0.mongodb.net/Base1.Cluster0?retryWrites=true&w=majority`
+
+  MONGO_URI: `mongodb+srv://sasha-is-permi:qwe123***@cluster1.v2afi.mongodb.net/sasha-is-permi?retryWrites=true&w=majority`
+
 }
